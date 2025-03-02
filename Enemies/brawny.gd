afcc40b20ae5f042159d23fbf2aadc9d323350fa
sdubois2 <111801641+sdubois2@users.noreply.github.com>
2025-03-02 02:34:42 -0600

    sum

--- Enemies/brawny.gd
+++ Enemies/brawny.gd
@@ -1,4 +1,7 @@
 extends CharacterBody2D
+
+var enemy_death_effect = preload("res://enemy_death_effect.tscn")
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var timer: Timer = $Timer
 
@@ -128,6 +131,11 @@ func _on_hurt_box_area_entered(area: Area2D) -> void:
 		var node = area.get_parent() as Node
 		health_amount -= node.damage_amount
 		print("Health amount: ", health_amount)
+		if health_amount <= 0:
+			var enemy_death_effect_instance = enemy_death_effect.instantiate() as Node2D
+			enemy_death_effect_instance.global_position = global_position
+			get_parent().add_child(enemy_death_effect_instance)
+			queue_free()
 
 func _on_hurt_box_body_entered(body: Node2D) -> void:
 	pass
